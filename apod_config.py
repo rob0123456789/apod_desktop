3af1ed4b4011090e173a9a1bdd1051ce450aa6fb
rob0123456789 <robert.panetta@gmail.com>
2022-11-09 19:39:06 -0500

    Modified the configuration page with better information

---
+++ apod_config.py
@@ -0,0 +1,11 @@
+# Generate your API key at https://api.nasa.gov/
+api_key = ''
+
+# Define the location of the downloaded image file
+image_file = ''
+
+# Define the location of the HTML shortcut 
+html_file = ''
+
+# Command for setting the desktop background
+destination = '' 
